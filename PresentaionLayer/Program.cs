9d8b5552b900f230b2a0267fe61846cf9b46112e
Ayman_Samir <111081687+aymansamiryoussef@users.noreply.github.com>
2024-11-27 18:23:13 +0200

    Add files via upload

---
+++ PresentaionLayer/Program.cs
@@ -0,0 +1,65 @@
+using BusinessLayer.Interface;
+using BusinessLayer.Reprosetory;
+using DataAccessLayer;
+using DataAccessLayer.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace WEBDEMO
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            
+            builder.Services.AddDbContext<ApplicationContextClass> (options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("CS"));
+
+
+            } );
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationContextClass>();
+            builder.Services.AddScoped<IAuthenticationRepository, AuthenticationReprosatory>();
+            builder.Services.AddScoped<ICategoryReprosatory,CatogeryReprosatory>();
+            builder.Services.AddScoped<IProudctReprository,ProudctReprosatory>();
+            builder.Services.AddDistributedMemoryCache();
+            
+
+            builder.Services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(20);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+                
+
+            });
+
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseSession();
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Proudct}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
