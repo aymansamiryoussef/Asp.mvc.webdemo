4cbd647d12631a9c36b1a638656f61944dac6c05
Ayman_Samir <111081687+aymansamiryoussef@users.noreply.github.com>
2024-11-27 18:13:44 +0200

    Add files via upload

---
+++ DataAccessLayer/ApplicationContextClass.cs
@@ -0,0 +1,42 @@
+﻿using DataAccessLayer.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayer
+{
+    public class ApplicationContextClass :IdentityDbContext<ApplicationUser>
+    {
+        
+
+
+        public ApplicationContextClass(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Proudct> Proudcts { get; set;}
+
+        public DbSet<Category> categories { get; set;}
+
+        public DbSet<Order> orders { get; set;}
+
+        public DbSet<Orderitem> orderitem { get; set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Order>()
+                .HasMany(o => o.Items)
+                .WithOne(oi => oi.Order)
+                .HasForeignKey(oi => oi.OrderID);
+
+
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+
+
+
+    }
+}
